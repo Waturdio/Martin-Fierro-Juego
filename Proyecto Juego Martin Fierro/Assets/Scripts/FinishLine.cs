7d5cea70cb236b82d9600ab69a4f266adaef0540
alvarsar <131689640+alvarsar@users.noreply.github.com>
2023-07-11 14:34:24 -0300

    Added: Linea de meta, objetos interactuables

---
+++ Proyecto Juego Martin Fierro/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    [SerializeField] GameObject finishPanel;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            finishPanel.SetActive(true);
+            GameObject.FindWithTag("MainCamera").transform.SetParent(null);
+            Destroy(other.gameObject);
+
+        }
+    }
+
+}
---
+++ Proyecto Juego Martin Fierro/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    [SerializeField] GameObject text;
+    [SerializeField] GameObject objetoJugador;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            text.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            text.SetActive(false);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                objetoJugador.SetActive(true);
+                text.SetActive(false);
+                Destroy(gameObject);
+            }
+    }
+}
